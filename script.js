b18975d7fe4a93064a66065cbc9d50931f8758d7
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-10 12:01:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function fibonacci(num) {
 // your code here
+	let f1=0, f2=1, sum=0;
+
+	while(num>0){
+		// console.log(f1 + " ");
+		sum = f1+f2;
+		f1 = f2;
+		f2 = sum;
+		num--;
+	}
+	console.log(f1);
 }
+
+// fibonacci(5);
 
 module.exports = fibonacci;
